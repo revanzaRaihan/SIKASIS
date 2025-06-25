df58792b25fa29447c5ce14cda43a73ba0c455f3
Revanza Raihan Rizqullah <revanzaraihanrizqullah@gmail.com>
2025-06-25 08:12:10 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const cacheName = 'kas-siswa-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
